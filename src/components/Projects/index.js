0a94ce1eaf99841c03dd85f13f0e42b0d34ccbda
Yasir Jafar <yasir991925@gmail.com>
2022-04-03 23:32:08 +0530

    responsive fix 1

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -22,7 +22,7 @@ function Projects({ data, landing }) {
   const tl = gsap.timeline()
   const project_image_deviation_y = 5
 
-  let cols = window && window.screen.width < 800 ? 1 : 3
+  let cols = 3
 
   useEffect(() => {
     cols = window && window.screen.width < 800 ? 1 : 3
